c71678a1523d48d6afa935b461d5c2af058d9dec
Bruno Marques <bruno.laerte@radixeng.com.br>
2024-07-30 17:58:00 -0300

    ✨feat: add bootstrap app

--- src/main.rs
+++ src/main.rs
@@ -8,3 +8,25 @@ struct Cli {
     size: usize,
     file: PathBuf,
 }
+
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("Erro: {}", e);
+    }
+}
+
+fn run() -> io::Result<()> {
+    let args = parse_args();
+    let file_size_in_bytes = calculate_file_size_in_bytes(args.size);
+
+    let file = create_file(&args.file)?;
+    write_to_file(file, file_size_in_bytes)?;
+
+    println!(
+        "Arquivo {} com tamanho de {} MB foi criado.",
+        args.file.display(),
+        args.size
+    );
+
+    Ok(())
+}
